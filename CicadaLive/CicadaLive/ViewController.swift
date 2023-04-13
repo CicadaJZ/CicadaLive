5be35df450532fc17fa418182bfa827ab2fcfa1e
CicadaJZ <cicadav@126.com>
2023-04-13 12:34:57 +0800

    初始化项目

---
+++ CicadaLive/CicadaLive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CicadaLive
+//
+//  Created by 知了 on 2023/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
