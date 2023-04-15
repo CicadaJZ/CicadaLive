122a7e869bb909ec02c3236eb68ccc5bc85e4118
CicadaJZ <cicadav@126.com>
2023-04-15 14:49:12 +0800

    全屏pop

--- CicadaLive/CicadaLive/Classes/Room/Controller/RoomNormalViewController.swift
+++ CicadaLive/CicadaLive/Classes/Room/Controller/RoomNormalViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class RoomNormalViewController: UIViewController, UIGestureRecognizerDelegate {
+class RoomNormalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
